fb2b173d52d557ec877dda9bd92ae4b8ab1ce668
acce8711 <101413675+acce8711@users.noreply.github.com>
2023-02-05 11:45:30 -0500

    finished up html and worked a bit on js

--- hangman V2/script.js
+++ hangman V2/script.js
@@ -3,23 +3,61 @@ const numThemes = 6;
 
 //Generating theme buttons
 
+
+
+
+
+
 const themes = [
     ["Theme 1", "Theme 2", "Theme 3", "Theme 4", "Theme 5", "Theme 6"],
     [1, 2, 3, 4, 5, 6]
 ];
 
-//test - access elements from array
-console.log(themes[1][0]);
+$(document).ready(function(){
+    //jquery will monitor if a theme button has been clicked
+    $(".themeButton").click(function () {
+        //getting the value of the clicked theme button
+        selectedTheme = $(this).val();
+        console.log(selectedTheme);
+    });
+})
 
-for (let i = 0; i < numThemes; i++) {
-    const themeButton = document.createElement("button");
-    const node = document.createTextNode(themes[0][i]);
-    themeButton.appendChild(node);
+generateThemeButtons();
+generateAlphabetButtons();
 
-    themeButton.value = themes[1][i];
-    themeButton.id = themes[1][i];
-    themeButton.className = "ThemeButton";
+function generateThemeButtons() {
+    //for loop creates 6 theme buttons and inserts them into the themeBox div in the html file
+    for (let i = 0; i < numThemes; i++) {
+        //creating a button element and assigning attribute values
+        const themeButton = document.createElement("button");
+        const node = document.createTextNode(themes[0][i]);
+        themeButton.appendChild(node);
+        themeButton.value = themes[1][i];
+        themeButton.id = themes[1][i];
+        themeButton.className = "themeButton";
 
-    const element = document.getElementById("themeBox");
-    element.appendChild(themeButton);
+        //appending the button to the html 
+        const element = document.getElementById("themeBox");
+        element.appendChild(themeButton);
+    }
 }
+
+function generateAlphabetButtons() {
+    for (let i = 65; i < 91; i++) {
+        const alphabetButton = document.createElement("button");
+        const currentLetter = String.fromCharCode(i);
+        const node = document.createTextNode(currentLetter);
+        
+        alphabetButton.appendChild(node);
+        alphabetButton.value = currentLetter;
+        alphabetButton.id = currentLetter;
+        alphabetButton.className = "alphabetLetter";
+
+        //appending the button to the html 
+        const element = document.getElementById("alphabetItems");
+        element.appendChild(alphabetButton);
+    }
+}
+
+
+
