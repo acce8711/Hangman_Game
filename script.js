d5f9372392c32973e54c2f5ccee6e4405c91ebc7
acce8711 <101413675+acce8711@users.noreply.github.com>
2023-02-15 01:13:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -421,7 +421,7 @@ function countUniqueLetters(str)
 //function handles the bob blink's on gameOver container
 function bobBlink()
 {
-    $("#bobEyes").attr({"src":"visualRecources/bobEyesClosed.png"});
+    $("#bobEyes").attr({"src":"visualRecources/bobEyesClosed2.png"});
     setTimeout(bobDevastated, Math.random()*(800-100)+100);
 }
 
